0112af42169000563882a593c8a4993ad50ba7d1
vinceix <vincentxie12@gmail.com>
2015-07-29 18:01:37 -0400

    First commit

---
+++ js/controllers/controller.js
@@ -0,0 +1,40 @@
+app.controller('about-controller', ['$scope', function($scope) { 
+	$scope.tech = [
+  		{
+  			name: 'HTML',
+  			color: '#e74c3c'
+  		},
+  		{
+  			name: 'CSS',
+  			color: '#3498db'
+  		},
+  		{
+  			name: 'Javascript',
+  			color: '#9b59b6'
+  		},
+  		{
+  			name: 'Java',
+  			color: '#2c3e50'
+  		},
+  		{
+  			name: 'C',
+  			color: '#2ecc71'
+  		},
+  		{
+  			name: 'C#',
+  			color: '#f1c40f'
+  		},
+  		{
+  			name: 'Python',
+  			color: '#e67e22'
+  		},
+  		{
+  			name: 'Objective C',
+  			color: '#95a5a6'
+  		},
+  		{
+  			name: 'Assembly',
+  			color: '#2980b9'
+  		}
+  	];
+}]);
