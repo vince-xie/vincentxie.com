4fa9e9f517653c634e40438bdb65f8cb9e0545f0
Vincent Xie <vincentxie12@gmail.com>
2015-07-31 14:13:59 -0400

    Update controller.js

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -45,18 +45,19 @@ app.controller('projects-controller', ['$scope', function($scope) {
         name: 'Personal Website',
         github: 'https://github.com/vinceix/vincentxie.com',
         desc: 'The source code for this website, vincentxie.com. This website is built using Bootstrap, AngularJS, and the animations are done using JQuery.',
-        link: 'http://www.vincentxie.com/',
+        link: 'http://www.vincentxie.com/'
       },
       {
         name: 'Intro to CS Textbook',
         github: 'https://github.com/vinceix/Cache-Simulator',
         desc: 'An intro to computer science textbook that I contributed to writing. This textbook will supplement the intro courses offered at codehs.com.',
-        link: 'http://codehs.gitbooks.io/introcs/content/',
+        link: 'http://codehs.gitbooks.io/introcs/content/'
       },
       {
         name: 'Cache simulator',
         github: 'https://github.com/vinceix/Cache-Simulator',
         desc: 'Simulates L1 cache behavior using a least recently used replacement algorithm. Can simulate both write-through and write-back. Written in C. ',
+        link: ''
       }
     ];
   $scope.hasDemo = function(project){
